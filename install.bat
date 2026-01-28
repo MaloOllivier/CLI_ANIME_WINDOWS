909e6a0acecbb9a51e2075accb620adbee4e90a6
MaloOllivier <malo.ollivier@etudiant.univ-rennes.fr>
2026-01-28 23:05:52 +0100

    meilleurs couleurs pour les étapes

--- install.bat
+++ install.bat
@@ -10,24 +10,24 @@ echo --- Rapport d'erreurs du %date% %time% --- > error.log
 echo %B%[*] Installation du setup en cours...%R%
 
 :: Installation de Scoop (silencieux)
-echo %O%[1/5] %B%Installation de Scoop...%R%
+echo %O%[%R%1/5%O%] %B%Installation de Scoop...%R%
 powershell -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force" >nul 2>> error.log
 powershell -Command "Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression" >nul 2>> error.log
 echo %O%[%R%OK%O%] %V%Scoop installe.%R%
 
 :: Ajout du bucket (silencieux)
-echo %O%[2/5] %B%Ajout du bucket extras...%R%
+echo %O%[%R%2/5%O%] %B%Ajout du bucket extras...%R%
 call scoop bucket add extras >nul 2>> error.log
 echo %O%[%R%OK%O%] %V%Bucket extras ajoute.%R%
 
 :: Installation des logiciels (silencieux)
 :: C'est ici que ça prend du temps, mais rien ne s'affichera
-echo %O%[3/5] %B%Installation des logiciels...%R%
+echo %O%[%R%3/5%O%] %B%Installation des logiciels...%R%
 call scoop install git mpv yt-dlp ffmpeg syncplay ani-cli aria2 >nul 2>> error.log
 echo %O%[%R%OK%O%] %V%Logiciels installes.%R%
 
 :: --- CONFIGURATION MPV ---
-echo %O%[4/5] %B%Configuration de MPV...%R%
+echo %O%[%R%4/5%O%] %B%Configuration de MPV...%R%
 set "MPV_TARGET=%USERPROFILE%\scoop\apps\mpv\current\portable_config"
 if not exist "%MPV_TARGET%" mkdir "%MPV_TARGET%" >nul 2>> error.log
 
@@ -38,7 +38,7 @@ del "%MPV_TARGET%\mpv.zip" >nul 2>> error.log
 echo %O%[%R%OK%O%] %V%Configuration MPV appliquee.%R%
 
 :: --- CONFIGURATION SYNCPLAY ---
-echo %O%[5/5] %B%Configuration de Syncplay...%R%
+echo %O%[%R%5/5%O%] %B%Configuration de Syncplay...%R%
 set "SYNCPLAY_PERSIST=%USERPROFILE%\scoop\persist\syncplay"
 if not exist "%SYNCPLAY_PERSIST%" mkdir "%SYNCPLAY_PERSIST%" >nul 2>> error.log
 copy /y "config\syncplay.ini" "%SYNCPLAY_PERSIST%\syncplay.ini" >nul 2>> error.log
