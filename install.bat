a56a1de7652669d432d08735c7a9e1db21e72918
MaloOllivier <malo.ollivier@etudiant.univ-rennes.fr>
2026-01-29 13:19:57 +0100

    retour en arriere, mais avec plus d'options d'affichage

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
 @echo off
 
-title "Installation du setup [0/5]""
+title Installation du setup [0/5]
 
 set "ESC="
 set "V=%ESC%[38;2;172;252;151m"
@@ -13,7 +13,7 @@ echo --- Rapport d'erreurs du %date% %time% --- > error.log
 echo %B%[*] Installation du setup en cours...%R%
 
 :: Installation de Scoop (silencieux)
-title "Installation du setup [1/5]""
+title Installation du setup [1/5]
 echo %O%[%R%1/5%O%] %B%Installation de Scoop...%R%
 powershell -Command "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force" >nul 2>> error.log
 powershell -Command "Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression" >nul 2>> error.log
@@ -28,15 +28,15 @@ call scoop bucket add extras >nul 2>> error.log
 echo %O%[%R%OK%O%]%R% %V%Bucket extras ajoute.%R%
 
 :: Installation des logiciels (silencieux)
-title "Installation du setup [3/5]""
+title Installation du setup [3/5]
 echo %O%[%R%3/5%O%] %B%Installation des logiciels...%R%
 
-start "Installation des logiciels" /wait powershell -ExecutionPolicy Bypass -File "scoopDownloads.ps1"
+start "Installation des logiciels" /wait powershell -Command "& { $scoop = '%USERPROFILE%\scoop\shims\scoop.cmd'; & $scoop install aria2 git mpv yt-dlp ffmpeg syncplay ani-cli 2>&1 | ForEach-Object { if ($_ -match 'was installed successfully!') { Write-Host $_.ToString().Trim() -ForegroundColor Green } elseif ($_ -match 'Download: \[#') { Write-Host $_.ToString().Replace('Download: ', '') } elseif ($_ -match 'Extracting') { Write-Host $_.ToString().Trim() } elseif ($_ -match 'Download: \(OK\)') { Write-Host $_.ToString().Trim() } }; timeout 3 }"
 
 echo %O%[%R%OK%O%]%R% %V%Logiciels installes.%R%
 
 :: --- CONFIGURATION MPV ---
-title "Installation du setup [4/5]""
+title Installation du setup [4/5]
 echo %O%[%R%4/5%O%] %B%Configuration de MPV...%R%
 
 set "MPV_TARGET=%USERPROFILE%\scoop\apps\mpv\current\portable_config"
@@ -49,7 +49,7 @@ del "%MPV_TARGET%\mpv.zip" >nul 2>> error.log
 echo %O%[%R%OK%O%]%R% %V%Configuration MPV appliquee.%R%
 
 :: --- CONFIGURATION SYNCPLAY ---
-title "Installation du setup [5/5]""
+title Installation du setup [5/5]
 echo %O%[%R%5/5%O%] %B%Configuration de Syncplay...%R%
 
 set "SYNCPLAY_PERSIST=%USERPROFILE%\scoop\persist\syncplay"
