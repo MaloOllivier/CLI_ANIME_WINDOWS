6583b1225be221e3d2fafbe9d576083d83d9b0c2
MaloOllivier <malo.ollivier@etudiant.univ-rennes.fr>
2026-01-29 07:21:36 +0100

    Patch avec path lecteur automatique

--- install.bat
+++ install.bat
@@ -17,17 +17,22 @@ echo %O%[%R%OK%O%] %V%Scoop installe.%R%
 
 :: Ajout du bucket (silencieux)
 echo %O%[%R%2/5%O%] %B%Ajout du bucket extras...%R%
+
 call scoop bucket add extras >nul 2>> error.log
+
 echo %O%[%R%OK%O%] %V%Bucket extras ajoute.%R%
 
 :: Installation des logiciels (silencieux)
 :: C'est ici que ça prend du temps, mais rien ne s'affichera
 echo %O%[%R%3/5%O%] %B%Installation des logiciels...%R%
+
 call scoop install git mpv yt-dlp ffmpeg syncplay ani-cli aria2 >nul 2>> error.log
+
 echo %O%[%R%OK%O%] %V%Logiciels installes.%R%
 
 :: --- CONFIGURATION MPV ---
 echo %O%[%R%4/5%O%] %B%Configuration de MPV...%R%
+
 set "MPV_TARGET=%USERPROFILE%\scoop\apps\mpv\current\portable_config"
 if not exist "%MPV_TARGET%" mkdir "%MPV_TARGET%" >nul 2>> error.log
 
@@ -39,9 +44,14 @@ echo %O%[%R%OK%O%] %V%Configuration MPV appliquee.%R%
 
 :: --- CONFIGURATION SYNCPLAY ---
 echo %O%[%R%5/5%O%] %B%Configuration de Syncplay...%R%
+
 set "SYNCPLAY_PERSIST=%USERPROFILE%\scoop\persist\syncplay"
 if not exist "%SYNCPLAY_PERSIST%" mkdir "%SYNCPLAY_PERSIST%" >nul 2>> error.log
 copy /y "config\syncplay.ini" "%SYNCPLAY_PERSIST%\syncplay.ini" >nul 2>> error.log
+
+powershell -Command "(Get-Content '%SYNCPLAY_PERSIST%\syncplay.ini') -replace 'playerpath = NULL', 'playerpath = %USERPROFILE%\scoop\apps\mpv\current\mpv.exe' | Set-Content '%SYNCPLAY_PERSIST%\syncplay.ini'" >nul 2>> error.log
+
+
 echo %O%[%R%OK%O%] %V%Configuration Syncplay appliquee.%R%
 
 echo.
